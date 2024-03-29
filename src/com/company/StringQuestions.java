855d6fd080f619e5d2f1c6495b455b4e407eabf6
anushasharma384 <sharmaanusha384@gmail.com>
2024-03-29 18:37:56 +0530

    String Questions

---
+++ src/com/company/StringQuestions.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Scanner;
+public class StringQuestions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //problem1
+        System.out.println("Please enter your name: ");
+        String name = sc.nextLine();
+        System.out.printf("Dear %s thanks a lot.", name);
+
+
+        //problem2
+        //detect double and tripple spaces in a string
+        System.out.println("Enter a string with double and tripple spaces: ");
+        String str = sc.nextLine();
+        System.out.println(str.indexOf("  "));
+        System.out.println(str.indexOf("   "));
+
+
+        //problem3
+        //
+    }
+}
