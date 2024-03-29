26a5c38f205e1acffc4937593782429dcc4d9cb9
anushasharma384 <sharmaanusha384@gmail.com>
2024-03-30 03:23:33 +0530

    Array Introduction

---
+++ src/com/company/MultiDimensionalArray.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class MultiDimensionalArray {
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/company/arrays.java
+++ src/com/company/arrays.java
@@ -1,7 +1,35 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class arrays {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+
+        //creating an array
+
+        //1
+        int[] marks;                          //declaration
+        marks = new int[5];                   //memory allocation
+
+        //2
+        int[] roll = new int[5];              //declaration + memory allocation
+
+        //3
+        int[] age = {10, 20, 30, 40, 50};     //declaration + initialization
+
+
+        //Getting elements into an array
+        int a = 90;
+        for(int i = 0; i < 5; i++){
+            marks[i] = a+i;
+        }
+
+
+        //Accessing elements of the array
+        for(int i = 0; i < 5; i++){
+            System.out.println(marks[i]);
+        }
     }
 }
