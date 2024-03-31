4090e53f137aedbf49141b3abbda404bf02993e5
anushasharma384 <sharmaanusha384@gmail.com>
2024-03-31 23:03:47 +0530

    Multi-dimensional Array

--- src/com/company/MultiDimensionalArray.java
+++ src/com/company/MultiDimensionalArray.java
@@ -1,7 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class MultiDimensionalArray {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int n = sc.nextInt();
+        System.out.println("Enter number of columns:");
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        System.out.println("Enter the element for the given info where elemnt is for arr[row][column].");
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.printf("Enter the element you want at arr[%d][%d]\n",i, j);
+                int el = sc.nextInt();
+                el = arr[i][j];
+            }
+        }
     }
 }
