8c519ba66129bd89bac989912e7eab36a4872080
anushasharma384 <sharmaanusha384@gmail.com>
2024-04-04 00:51:31 +0530

    Variable Arguments

---
+++ src/com/company/VarArgs.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class VarArgs {
+    //In this the number of arguments stated in a method(function) are variable
+    static int sum(int...arr){
+        //available as int[] arr
+        int res = 0;
+        for(int a : arr){
+            res = res + a;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        System.out.println("The sum of 4 and 5 is: " + sum(4,5));
+        System.out.println("The sum of 4, 5, and 6 is: " + sum(4,5,6));
+        System.out.println("The sum of 4, 5, 6, and 10 is: " + sum(4,5,6,10));
+    }
+}
