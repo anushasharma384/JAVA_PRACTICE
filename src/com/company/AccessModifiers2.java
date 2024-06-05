126a4baec1fbb41d36306ea366378e91634dadb2
anushasharma384 <sharmaanusha384@gmail.com>
2024-06-05 20:17:50 +0530

    Threads display concurrency

--- src/com/company/AccessModifiers2.java
+++ src/com/company/AccessModifiers2.java
@@ -1,4 +1,9 @@
 package com.company;
+//We can use any access modifier in same class.
+//Public modifier can be used anywhere class, package, subclass and even anywhere in the world.
+//Protected modifier can be used in same class, same package subclass but not anywhere in world.
+//Default(no) modifier can be used only in same class and package.
+//Private modifier can only be used in same class. Other classes can not call that modifier directly but can access it using the method in that same class.
 
 class modifiers{
     public int x = 5;
---
+++ src/com/company/Threads.java
@@ -0,0 +1,37 @@
+package com.company;
+//Concurrency is the task of running and managing multiple computations at the same time.
+//Parallelism is the task of running and managing multiple computations simultaneously.
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        int i = 0;
+        while(i<40000){
+            System.out.println("Running MyThread");
+            i++;
+        }
+    }
+}
+class MyThread2 extends Thread{
+    @Override
+    public void run() {
+        int i = 0;
+        while(i<40000){
+            System.out.println("Running MyThread 2");
+            i++;
+        }
+    }
+}
+
+public class Threads {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread2 t2 = new MyThread2();
+        //start method is used to call the methods of the thread.
+        t1.start();
+        t2.start();
+        //when we see the output we can clearly see that processor is dividing  time between t1 and t2
+        //It can not run t1 and t2 simultaneously.
+        //Hence we can say that threads are based on concurrency.
+    }
+}
+
