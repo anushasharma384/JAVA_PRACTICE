0e7f9b655042be37862ef4a9c65044c90e8ec5ee
anushasharma384 <sharmaanusha384@gmail.com>
2024-06-20 16:37:54 +0530

    LinkedList

---
+++ src/com/company/Linkedlist.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.LinkedList;
+class Node{
+    int data;
+    Node next;
+    Node(int data1, Node next1){
+        this.data = data1;
+        this.next = next1;
+    }
+    Node(int data1){
+        this.data = data1;
+        this.next = null;
+    }
+}
+public class Linkedlist {
+    public static void main(String[] args) {
+        Linkedlist l = new Linkedlist();
+
+    }
+}
